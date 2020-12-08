9b7673415eb6fba82369b01b274a0f54d771d020
jinxluck <54708834+jinxluck@users.noreply.github.com>
2020-12-08 14:45:36 +0100

    Update helloworldmmj.c

--- helloworldmmj.c
+++ helloworldmmj.c
@@ -2,7 +2,13 @@
 
 int main()
 {
-	printf("Hello World mmj test take2 \n");
+	int count = 0;
+	
+	for(count=0;count<10;count++)
+	{
+		printf("Hello World mmj test take2 \n");
+		print("%d",count);
+	}
 
 	return 0;
 }
